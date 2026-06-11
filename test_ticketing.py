1351fd3ba375f3bcc85853a0c3b7355eeb28bba9
khanhtoan10 <138018914+khanhtoan10@users.noreply.github.com>
2026-06-11 10:05:38 +0700

    Add files via upload

--- test_ticketing.py
+++ test_ticketing.py
@@ -1,19 +1,14 @@
-import unittest
 from main import calculate_revenue
 
 
-class TestTicketing(unittest.TestCase):
-    def test_mixed_tickets(self):
-        tickets = [
-            {"price": 100.0, "status": "Booked"},
-            {"price": 200.0, "status": "Cancelled"},
-            {"price": 300.0, "status": "Booked"}
-        ]
-        self.assertEqual(calculate_revenue(tickets), 400.0)
-
-    def test_empty_list(self):
-        self.assertEqual(calculate_revenue([]), 0.0)
+def test_mixed_tickets():
+    tickets = [
+        {"price": 100.0, "status": "Booked"},
+        {"price": 200.0, "status": "Cancelled"},
+        {"price": 300.0, "status": "Booked"}
+    ]
+    assert calculate_revenue(tickets) == 400.0
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_empty_list():
+    assert calculate_revenue([]) == 0.0
